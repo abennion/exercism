c1af62792e98784ed5cdc7ea30fc1472d2d4f1b7
Alex Bennion <spam281@gmail.com>
2016-07-22 11:08:50 -0500

    saving changes

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,55 +1,61 @@
 'use strict';
 module.exports = class {
   push(value) {
-    console.log('pushing ' + value);
     let node = {value: value};
     if (this.last !== undefined) {
-      node['previous'] = this.last;
-      this.last['next'] = node;
+      node.previous = this.last;
+      this.last.next = node;
     }
     this.last = node;
     if (this.first === undefined)
       this.first = node;
-    console.log('pushed: ' + this.toString());
   }
 
   pop() {
     let value = this.last.value;
-    console.log('popping [' + value + ']');
     this.last = this.last.previous;
-    // last can be empty, if the queue is empty
-    // do we keep a counter????
     if (this.last !== undefined)
-      this.last['next'] = undefined;
-    console.log('popped: ' + this.toString());
+      this.last.next = undefined;
     return value;
   }
 
   shift() {
     let value = this.first.value;
-    console.log('shifting [' + value + ']');
     this.first = this.first.next;
     if (this.first !== undefined)
-      this.first['previous'] = undefined;
-    console.log('shifted: ' + this.toString());
-    return value;  
+      this.first.previous = undefined;
+    return value;
   }
 
   unshift(value) {
-    console.log('unshifting ' + value);
     let node = {value: value};
     if (this.first !== undefined) {
-      node['next'] = this.first;
-      this.first['previous'] = node;
+      node.next = this.first;
+      this.first.previous = node;
     }
     this.first = node;
     if (this.last === undefined)
       this.last = node;
-    console.log('unshifted: ' + this.toString());
   }
 
-  delete() {
-    
+  delete(value) {
+    let count = this.count();
+    if (count === 0)
+      return;
+    let n = this.last;
+    while (n !== undefined) {
+      if (n.value === value) {
+        if (n.previous !== undefined)
+          n.previous.next = n.next;
+        if (n.next !== undefined)
+          n.next.previous = n.previous;
+        if (n === this.last)
+          this.last = undefined;
+        if (n === this.first)
+          this.first = undefined;
+      }
+      n = n.previous;
+    }
   }
 
   count() {
@@ -60,15 +66,5 @@ module.exports = class {
       n = n.next;
     }
     return c;
-  }
-
-  toString() {
-    let n = this.first;
-    let s = '';
-    while (n !== undefined) {
-      s += '[' + n.value + ']';
-      n = n.next;
-    }
-    return s;
   }
 };
