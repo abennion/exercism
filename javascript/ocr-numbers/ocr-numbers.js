4da13414dad429f0c25c6ed2b86cc99c630d6332
Alex Bennion <spam281@gmail.com>
2016-08-04 12:42:45 -0500

    saving

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,17 +1,4 @@
 'use strict';
-/*const m = {
-  0: ' _ \n| |\n|_|\n   ',
-  1: '   \n  |\n  |\n   ',
-  2: ' _ \n _|\n|_ \n   ',
-  3: ' _ \n _|\n _|\n   ',
-  4: '   \n|_|\n  |\n   ',
-  5: ' _ \n|_ \n _|\n   ',
-  6: ' _ \n|_ \n|_|\n   ',
-  7: ' _ \n  |\n  |\n   ',
-  8: ' _ \n| |\n|_|\n   ',
-  9: ' _ \n|_|\n  |\n   '
-}*/
-
 const m = {
   '-93577388': '0',
   '1953016154': '1',
@@ -23,7 +10,7 @@ const m = {
   '167593851': '7',
   '410973685': '8',
   '365013657': '9'
-}
+};
 
 module.exports = class {
   static hashCode(str) {
@@ -31,76 +18,38 @@ module.exports = class {
     if (str.length === 0)
       return hash;
     for (let i = 0; i < str.length; i++) {
-        var ord = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + ord;
-        hash = hash & hash;
+      var ord = str.charCodeAt(i);
+      hash = ((hash << 5) - hash) + ord;
+      hash &= hash;
     }
     return hash;
   }
 
   static parseNumber(s) {
-    console.log('parseNumber(' + s + ')...');
     let k = this.hashCode(s).toString();
     if (k in m)
       return m[k];
     return '?';
   }
 
-/*
-  let s = '    _  _ \n' +
-      '  | _| _|\n' +
-      '  ||_  _|\n' +
-      '         \n' +
-      '    _  _ \n' +
-      '|_||_ |_ \n' +
-      '  | _||_|\n' +
-      '         \n' +
-      ' _  _  _ \n' +
-      '  ||_||_|\n' +
-      '  ||_| _|\n' +
-      '         ';
-*/
-
   static convert(s) {
-    let m = s.match(/[\ ]{4,}/g);
-    if (m) {
-      m = m.sort((a, b) => b.length - a.length);
-      s = s.split(m[0]).filter(e => /[\|_]/.test(e));;
-    }
-    let parts = [].concat(s);
-    //console.log('parts: ' + parts);
-
-    let res = parts.reduce((retVal, nums, i) => {
-      console.log('nums: `' + nums + '`');
-      nums = nums.split('\n').filter(e => /[\|_]/.test(e)).map(e => e.match(/.{1,3}/g));
-      console.log('nums[0]: `' + nums[0] + '`');
-      console.log('nums: `' + nums + '`');
-      console.log('nums.length: ' + nums.length);
-      if (nums.length > 1) {
-        // still a leading \n after the split!!!
-        // transpose
-        nums = nums[0].map((num, i) => {
-          // ' _ ', ' _ ', ' _ '
-          return num.split('').map((col, j) => {
-            return nums[i][j];
-          }).join('');
-        }).join('\n') + '\n';
+    let rows = s.split('\n').map(e => e.split(''));
+    let res = '';
+    while (rows.length > 0) {
+      let num = ['', '', '', ''];
+      for (let r = 0; r < 4; r++) {
+        for (let c = 0; c < 3; c++) {
+          num[r] += rows[r].shift();
+        }
       }
-      console.log('nums(2): `' + nums + '`');
-      return;
-
-      nums = [].concat(nums);
-      // parse nums 
-      nums.map(num => {
-        let k = this.hashCode(num).toString();
-        console.log('num: ' + num);
-        if (k in m)
-          return m[k];
-        return '?';
-      });
-    }, '');
-
-    console.log('res: ' + res);
+      num = num.join('\n');
+      res += this.parseNumber(num);
+      if (rows[0].length === 0) {
+        rows = rows.slice(4);
+        if (rows.length > 0)
+          res += ',';
+      }
+    }
     return res;
   }
 };
