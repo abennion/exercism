77868f9c135ca40f4bae378c2042784179352277
Alex Bennion <alex.bennion@gmail.com>
2018-05-03 20:52:53 -0500

    saving

--- go/parallel-letter-frequency/letter.go
+++ go/parallel-letter-frequency/letter.go
@@ -1,18 +1,15 @@
 package letter
 
-// FrequencyProducer ...
-func FrequencyProducer(ss []string, c chan FreqMap) {
-	for _, s := range ss {
-		c <- Frequency(s)
-	}
-	close(c)
-}
-
 // ConcurrentFrequency ...
 func ConcurrentFrequency(ss []string) FreqMap {
 	c := make(chan FreqMap)
+	go func() {
+		for _, s := range ss {
+			c <- Frequency(s)
+		}
+		close(c)
+	}()
 	res := FreqMap{}
-	go FrequencyProducer(ss, c)
 	for fm := range c {
 		for k, v := range fm {
 			res[k] += v
