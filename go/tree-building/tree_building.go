4fd56c0027f3a39cab791f3ef524076431ba51e0
Alex Bennion <alex.bennion@gmail.com>
2018-05-04 16:51:25 -0500

    backing up

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -5,37 +5,35 @@ import (
 	"sort"
 )
 
-// Record ...
+// Record of a parent-child relationship.
 type Record struct {
 	ID, Parent int
 }
 
-// Node ...
+// Node of a tree.
 type Node struct {
 	ID       int
 	Children []*Node
 }
 
-// Lookup ...
-type Lookup map[int]*Node
+// NodeMap is a map of Nodes by ID.
+type NodeMap map[int]*Node
 
-// LookupNode ...
-func LookupNode(ID int, lookup Lookup) *Node {
-	if n, ok := lookup[ID]; ok {
+// GetNode returns a Node found in the specified NodeMap or a new Node.
+func GetNode(ID int, nodes NodeMap) *Node {
+	if n, ok := nodes[ID]; ok {
 		return n
 	}
-	node := &Node{ID: ID}
-	lookup[ID] = node
-	return node
+	return &Node{ID: ID}
 }
 
-// Build a tree of Nodes from an unordered list of parent-child records.
+// Build returns a tree of Nodes from an unordered list of parent-child records.
 func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	lookup := make(map[int]*Node)
+	lookup := NodeMap{}
 	var root *Node
 	var ids []int
 
@@ -45,142 +43,40 @@ func Build(records []Record) (*Node, error) {
 		}
 
 		ids = append(ids, r.ID)
-		node := LookupNode(r.ID, lookup)
+		node := GetNode(r.ID, lookup)
+		lookup[r.ID] = node
 
 		if r.ID == r.Parent {
 			if root != nil {
 				return nil, fmt.Errorf("duplicate root")
 			}
 			root = node
-		} else {
-			parent := LookupNode(r.Parent, lookup)
+			continue
+		}
 
-			// append node to parent
-			appended := false
-			l := len(parent.Children)
-			for i := 0; i < l; i++ {
-				child := parent.Children[i]
-				if node.ID > 100 {
-					return nil, fmt.Errorf("fucked up")
-				}
-				if node.ID == child.ID {
-					return nil, fmt.Errorf("duplicate node")
-				}
-				if node.ID < child.ID {
-					parent.Children = append(parent.Children[:i], append([]*Node{node}, parent.Children[i:]...)...)
-					appended = true
-					break
-				}
-			}
+		parent := GetNode(r.Parent, lookup)
+		lookup[r.Parent] = parent
 
-			if !appended {
-				parent.Children = append(parent.Children, node)
+		appended := false
+		for i, child := range parent.Children {
+			if node.ID == child.ID {
+				return nil, fmt.Errorf("duplicate node")
 			}
+			if node.ID < child.ID {
+				parent.Children = append(parent.Children[:i], append([]*Node{node}, parent.Children[i:]...)...)
+				appended = true
+				break
+			}
+		}
+		if !appended {
+			parent.Children = append(parent.Children, node)
 		}
 	}
 
-	// non-continuous means, we're missing an ID in order
 	sort.Ints(ids)
 	if ids[len(ids)-1] > len(ids)-1 {
 		return nil, fmt.Errorf("non-continuous")
 	}
 
-	if root == nil || root.ID != 0 {
-		return nil, fmt.Errorf("no root node")
-	}
-
-	// return the root node
 	return root, nil
 }
-
-// type Mismatch struct{}
-
-// func (m Mismatch) Error() string {
-// 	return "c"
-// }
-
-// func Build(records []Record) (*Node, error) {
-// 	if len(records) == 0 {
-// 		return nil, nil
-// 	}
-// 	root := &Node{}
-// 	todo := []*Node{root}
-// 	n := 1
-// 	for {
-// 		if len(todo) == 0 {
-// 			break
-// 		}
-// 		newTodo := []*Node(nil)
-// 		for _, c := range todo {
-// 			for _, r := range records {
-// 				if r.Parent == c.ID {
-// 					if r.ID < c.ID {
-// 						return nil, errors.New("a")
-// 					} else if r.ID == c.ID {
-// 						if r.ID != 0 {
-// 							return nil, fmt.Errorf("b")
-// 						}
-// 					} else {
-// 						n++
-// 						switch len(c.Children) {
-// 						case 0:
-// 							nn := &Node{ID: r.ID}
-// 							c.Children = []*Node{nn}
-// 							newTodo = append(newTodo, nn)
-// 						case 1:
-// 							nn := &Node{ID: r.ID}
-// 							if c.Children[0].ID < r.ID {
-// 								c.Children = []*Node{c.Children[0], nn}
-// 								newTodo = append(newTodo, nn)
-// 							} else {
-// 								c.Children = []*Node{nn, c.Children[0]}
-// 								newTodo = append(newTodo, nn)
-// 							}
-// 						default:
-// 							nn := &Node{ID: r.ID}
-// 							newTodo = append(newTodo, nn)
-// 						breakpoint:
-// 							for range []bool{false} {
-// 								for i, cc := range c.Children {
-// 									if cc.ID > r.ID {
-// 										a := make([]*Node, len(c.Children)+1)
-// 										copy(a, c.Children[:i])
-// 										copy(a[i+1:], c.Children[i:])
-// 										copy(a[i:i+1], []*Node{nn})
-// 										c.Children = a
-// 										break breakpoint
-// 									}
-// 								}
-// 								c.Children = append(c.Children, nn)
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 		todo = newTodo
-// 	}
-// 	if n != len(records) {
-// 		return nil, Mismatch{}
-// 	}
-// 	if err := chk(root, len(records)); err != nil {
-// 		return nil, err
-// 	}
-// 	return root, nil
-// }
-
-// func chk(n *Node, m int) (err error) {
-// 	if n.ID > m {
-// 		return fmt.Errorf("z")
-// 	} else if n.ID == m {
-// 		return fmt.Errorf("y")
-// 	} else {
-// 		for i := 0; i < len(n.Children); i++ {
-// 			err = chk(n.Children[i], m)
-// 			if err != nil {
-// 				return
-// 			}
-// 		}
-// 		return
-// 	}
-// }
