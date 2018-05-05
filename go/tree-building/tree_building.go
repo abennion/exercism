c30bacfb0c77b838e1601f2534428d4468b69274
Alex Bennion <alex.bennion@gmail.com>
2018-05-05 18:56:05 -0500

    backing up

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -2,7 +2,6 @@ package tree
 
 import (
 	"fmt"
-	"sort"
 )
 
 // Record of a parent-child relationship.
@@ -16,15 +15,22 @@ type Node struct {
 	Children []*Node
 }
 
-// NodeMap is a map of Nodes by ID.
-type NodeMap map[int]*Node
+type NodeData struct {
+	Node       *Node
+	FromRecord bool
+}
 
-// GetNode either returns a Node found in the specified NodeMap or a new Node.
-func GetNode(ID int, nodes NodeMap) *Node {
-	if n, ok := nodes[ID]; ok {
-		return n
+type NodeTable struct {
+	NodeMap map[int]NodeData
+}
+
+func (nt NodeTable) GetNode(ID int, FromRecord bool) *Node {
+	if nd, ok := nt.NodeMap[ID]; ok {
+		nt.NodeMap[ID] = NodeData{Node: nd.Node, FromRecord: nd.FromRecord || FromRecord}
+	} else {
+		nt.NodeMap[ID] = NodeData{Node: &Node{ID: ID}, FromRecord: FromRecord}
 	}
-	return &Node{ID: ID}
+	return nt.NodeMap[ID].Node
 }
 
 // Build returns a tree of Nodes from an unordered list of parent-child records.
@@ -33,29 +39,32 @@ func Build(records []Record) (*Node, error) {
 		return nil, nil
 	}
 
-	lookup := NodeMap{}
-	var root *Node
-	var ids []int
+	lookup := NodeTable{NodeMap: make(map[int]NodeData)}
 
 	for _, r := range records {
 		if r.ID < r.Parent {
 			return nil, fmt.Errorf("higher id parent of lower id")
 		}
 
-		ids = append(ids, r.ID)
-		node := GetNode(r.ID, lookup)
-		lookup[r.ID] = node
-
-		if r.ID == r.Parent {
-			if root != nil {
+		if r.ID == 0 {
+			nd, ok := lookup.NodeMap[r.ID]
+			if ok && nd.FromRecord {
 				return nil, fmt.Errorf("duplicate root")
 			}
-			root = node
+		} else {
+			if r.ID == r.Parent {
+				return nil, fmt.Errorf("nope")
+			}
+		}
+
+		node := lookup.GetNode(r.ID, true)
+
+		if r.ID == 0 {
 			continue
 		}
 
-		parent := GetNode(r.Parent, lookup)
-		lookup[r.Parent] = parent
+		// find duplicate node here
+		parent := lookup.GetNode(r.Parent, false)
 
 		appended := false
 		for i, child := range parent.Children {
@@ -73,10 +82,14 @@ func Build(records []Record) (*Node, error) {
 		}
 	}
 
-	sort.Ints(ids)
-	if ids[len(ids)-1] > len(ids)-1 {
+	last := len(lookup.NodeMap) - 1
+	if nd, ok := lookup.NodeMap[last]; !ok || nd.Node.ID != last {
 		return nil, fmt.Errorf("non-continuous")
 	}
 
-	return root, nil
+	nd, ok := lookup.NodeMap[0]
+	if !ok || !nd.FromRecord {
+		return nil, fmt.Errorf("no root node")
+	}
+	return nd.Node, nil
 }
