48046f8a6e34ac9f70a614c88ef28bbd4a94cf86
Alex Bennion <alex.bennion@gmail.com>
2018-08-07 09:51:38 -0500

    delete old elm solutions

--- elm/accumulate/Accumulate.elm
+++
@@ -1,8 +0,0 @@
-module Accumulate exposing (..)
-
-import List exposing (map)
-
-
-accumulate : (a -> b) -> List a -> List b
-accumulate f x =
-  map f x
--- elm/twelve-days/TwelveDays.elm
+++
@@ -1,6 +0,0 @@
-module TwelveDays exposing (recite)
-
-
-recite : Int -> Int -> List String
-recite start stop =
-    Debug.crash "Please implement this function"
