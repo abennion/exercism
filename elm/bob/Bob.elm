47d7a0d8bcb1230994946f4ac160139b6bb7f588
Alex Bennion <alex.bennion@gmail.com>
2018-08-07 19:17:48 -0500

    saving

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,6 +1,25 @@
 module Bob exposing (hey)
 
+import Char exposing (isUpper)
+import String exposing (any, right, toUpper, trim)
+
 
 hey : String -> String
 hey remark =
-    Debug.crash "Please implement this function"
+    let
+        isYell s =
+            any isUpper s && toUpper s == s
+
+        isQuestion s =
+            right 1 s == "?"
+    in
+    if trim remark == "" then
+        "Fine. Be that way!"
+    else if isYell remark && isQuestion remark then
+        "Calm down, I know what I'm doing!"
+    else if isYell remark then
+        "Whoa, chill out!"
+    else if isQuestion remark then
+        "Sure."
+    else
+        "Whatever."
